da2b293ac9c059e7f76f15242407942cddad8370
LewisAI93 <lewi.watson93@gmail.com>
2026-04-04 21:57:51 +0300

    chore: allow passing custom transforms to dataset

--- object_detection/dataset.py
+++ object_detection/dataset.py
@@ -4,11 +4,17 @@ from torchvision.transforms.functional import to_tensor
 import os
 from args import get_args
 from utils import resize_box_xyxy
+import augmentations as aug
 
 class ObjDetectionDataset(torch.utils.data.Dataset):
     
-    def __init__(self, df):
+    def __init__(self, df, transform=None):
         self.df = df.reset_index(drop=True)
+
+        if transform is None:
+            self.transform = aug.NoTransform()
+        else:
+            self.transform = aug.Compose(transform)
 
     def __len__(self):
         return len(self.df)
@@ -47,5 +53,7 @@ class ObjDetectionDataset(torch.utils.data.Dataset):
             "labels": torch.tensor(labels, dtype=torch.int64),
             "image_id": torch.tensor([idx]),
         }
+
+        image, target = self.transform(image, target)
 
         return image, target
