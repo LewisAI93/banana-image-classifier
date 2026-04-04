b56ad1a0acbc30367ce806eef0fe96952b4c6cf6
LewisAI93 <lewi.watson93@gmail.com>
2026-04-04 21:55:34 +0300

    chore: changed parameters to best model

--- object_detection/args.py
+++ object_detection/args.py
@@ -12,10 +12,11 @@ def get_args():
     
     parser.add_argument('--csv_dir', type=str, default='object_detection/data/CSVs')
     parser.add_argument('--out_dir', type=str, default='./sessions')
+    parser.add_argument('--plot_out_dir', type=str, default='./plots')
 
-    parser.add_argument('--batch_size', type=int, default=8, choices=[8,16,32,64])
+    parser.add_argument('--batch_size', type=int, default=16, choices=[8,16,32,64])
     parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--lr', type=float, default=0.001) # learning rate, adjust for learning curve
-    parser.add_argument('--wd', type=float, default=1e-4) # weight decay. Other options: 1e-5 or 1e-7
+    parser.add_argument('--lr', type=float, default=1e-5) # learning rate, adjust for learning curve
+    parser.add_argument('--wd', type=float, default=5e-4) # weight decay. Other options: 1e-5 or 1e-7
 
     return parser.parse_args()
